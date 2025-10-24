6b48e6f80e0eba3ad9de4a5d4799abe9befb3f8b
elvisscochito <elvirodominguezsoriano@outlook.com>
2025-10-24 16:51:45 -0600

    added new links to link-in-bio page

--- src/pages/LinkInBio.jsx
+++ src/pages/LinkInBio.jsx
@@ -1,5 +1,5 @@
 import { faFacebook, faInstagram, faWhatsapp } from '@fortawesome/free-brands-svg-icons';
-import { faFile, /* faGlobe, */ faLink, faLocationDot } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope, faFile, /* faGlobe, */ faLink, faLocationDot, faMotorcycle } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Logo from '../assets/Logo.svg';
 import styles from '../styles/LinkInBio.module.css';
@@ -17,6 +17,9 @@ function LinkInBio() {
         <a href="https://forms.gle/ahWSMCeZo9DzAVcx7" target="_blank" rel="noopener noreferrer">
           <FontAwesomeIcon icon={faFile} className={styles.icon} />&nbsp;Encuesta
         </a>
+        <a href="https://www.ubereats.com/store/corn-crunch-cuernavaca/rDzFFLTDQ1C_5HSHSqgPBA?diningMode=DELIVERY&pl=JTdCJTIyYWRkcmVzcyUyMiUzQSUyMkNvcm4lMjBDcnVuY2glMjIlMkMlMjJyZWZlcmVuY2UlMjIlM0ElMjJDaElKWXhxakNRRFp6WVVSOWRFRjdqUXl2TU0lMjIlMkMlMjJyZWZlcmVuY2VUeXBlJTIyJTNBJTIyZ29vZ2xlX3BsYWNlcyUyMiUyQyUyMmxhdGl0dWRlJTIyJTNBMTguODI4NjgyOSUyQyUyMmxvbmdpdHVkZSUyMiUzQS05OS4yMjc5MTI2JTdE&utm_source=menu-maker" target="_blank" rel="noopener noreferrer">
+          <FontAwesomeIcon icon={faMotorcycle} className={styles.icon} />&nbsp;Uber Eats Store
+        </a>
         <a href="https://www.instagram.com/corncrunch.mx/" target="_blank" rel="noopener noreferrer">
           <FontAwesomeIcon icon={faInstagram} className={styles.icon} />&nbsp;&#64;corncrunch.mx
         </a>
@@ -28,6 +31,9 @@ function LinkInBio() {
         </a>
         <a href="https://corncrunch.com">
           <FontAwesomeIcon icon={faLink} className={styles.icon} />&nbsp;corncrunch.com
+        </a>
+        <a href="mailto:contact@corncrunch.com">
+          <FontAwesomeIcon icon={faEnvelope} className={styles.icon} />&nbsp;contact@corncrunch.com
         </a>
       </div>
       <footer>
