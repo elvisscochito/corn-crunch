b9eedc8834d7f8f8ecbb8a36d041eb5e6c5e363f
elvisscochito <elvirodominguezsoriano@outlook.com>
2025-10-24 12:34:28 -0600

    trying to fix routing on deploy

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -6,7 +6,7 @@ const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route index path="/" element={<Landing />} />
+        <Route path="/" element={<Landing />} />
         <Route path="/link-in-bio" element={<LinkInBio />} />
       </Routes>
     </BrowserRouter>
