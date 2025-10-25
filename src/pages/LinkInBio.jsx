c5c440f75263618144d12c8bac2c14bba409bb1d
elvisscochito <elvirodominguezsoriano@outlook.com>
2025-10-25 11:21:57 -0600

    trying to fix overflow on x

--- src/pages/LinkInBio.jsx
+++ src/pages/LinkInBio.jsx
@@ -1,12 +1,8 @@
-import Logo from '../assets/Logo.svg';
 import styles from '../styles/LinkInBio.module.css';
 
 function LinkInBio() {
   return (
     <div className={styles.LinkInBio}>
-      <header>
-        <img src={Logo} alt="Logo" />
-      </header>
       <footer>
         <p>© 2025 Corn Crunch. Todos los derechos reservados.</p>
       </footer>
